065f73f0f70662440f9a5d5b38a5393e525d9b1f
Fedor_Sannikov <fedor.sannikov88@gmail.com>
2023-01-23 23:35:45 +0300

    finished homework #2

--- homework2/task1.java
+++ homework2/task1.java
@@ -9,10 +9,10 @@
 */
 
 /*
-Комментарий к условию задачи (не знаю кто это не знает):
+Комментарий к условию задачи:
 Наименьший по размеру целочисленный тип это byte (8-битовый тип)
 с диапазоном допустимых значений от -128 до 127.
-Конешно при вводе будет вызвана ошибка "Value out of range"
+При вводе 200 будет вызвана ошибка "Value out of range".
 */
 package homework2;
 
@@ -26,7 +26,7 @@ public class task1{
     public static void send_log(String text) throws IOException {
         
         Logger logger = Logger.getLogger(task1.class.getName());
-        FileHandler fh = new FileHandler("log.txt", true);
+        FileHandler fh = new FileHandler("log_for_task1.txt", true);
         logger.addHandler(fh);
         
         SimpleFormatter sFormat = new SimpleFormatter();
@@ -47,28 +47,28 @@ public class task1{
             terminal_scanner.close();
             data_str = Byte.toString(data);
         }
-        catch (Exception errorException) {
-            send_log(errorException.toString());
+        catch (Exception e) {
+            send_log(e.toString());
         }
         return data_str;
     }
 
-    public static void writing_data_in_file(String data_str) throws IOException {
-
+    public static void writing_data_in_txt_file(String data_str, String file_name) throws IOException {
+        
+        file_name +=".txt";
+    
         if (data_str == ""){
             return;
         }
     
-        try (FileWriter file_for_write = new FileWriter("result.txt", true)) {
+        try (FileWriter file_for_write = new FileWriter(file_name, true)) {
 
-            file_for_write.write(data_str);
-            file_for_write.append('\n');
+            file_for_write.write(data_str + "\n");
             file_for_write.flush();
+            file_for_write.close();
         }
-        catch (IOException errorException) {
-            send_log(errorException.getMessage());
-            //send_log(errorException.toString());
-
+        catch (IOException e) {
+            send_log(e.getMessage());
         }
     }
 
@@ -77,7 +77,9 @@ public class task1{
         String text = "Введите число в формате byte: ";
 
         String data_str = get_data_console_format_byte_return_string(text);
+        
+        String file_name = "result";
 
-        writing_data_in_file(data_str);
+        writing_data_in_txt_file(data_str,file_name);
     }
 }
